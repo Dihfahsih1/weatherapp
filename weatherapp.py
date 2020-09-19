18897824beec18578e125bbf4f505509698fedee
Dihfahsih1 <dihfahsihm@gmail.com>
2020-09-19 15:51:09 +0300

    i wanted to create the executable for this but for now pyinstall does not support python 3.8 which am currently using

--- weatherapp.py
+++ weatherapp.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import font
+import queue
 import requests
 HEIGHT = 500
 WIDTH = 600
@@ -23,29 +25,25 @@ def get_weather(city):
 
     label['text'] =format_response(weather)
 
-    
-    
-
-
 root=tk.Tk()
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
-background_image = tk.PhotoImage(file='app_web.png')
+background_image = tk.PhotoImage(file='landscape.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 frame = tk.Frame(root, bg='#80c1ff', bd=5 )
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 
-entry = tk.Entry(frame, font=40)
+entry = tk.Entry(frame, font=('Courier', 18))
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text ="Get Weather", font=40, command = lambda:get_weather(entry.get()))
+button = tk.Button(frame, text ="Get Weather", font=('Courier', 12), command = lambda:get_weather(entry.get()))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
 lower_frame = tk.Frame(root, bg='#80c1ff', bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-label = tk.Label(lower_frame, font=40)
+label = tk.Label(lower_frame, font=('Courier', 18), anchor = 'nw')
 label.place(relwidth=1, relheight=1)
 root.mainloop()
